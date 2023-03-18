ff7e845f0ffacfa91dd56e8f2ce3c1e3398bee31
Kabdolla <Madiyar.Kabdolla@kaspi.kz>
2023-03-18 20:43:48 +0600

    Designing and Testing Thread-safe Components with DispatchQueue, Serial vs. Concurrent Queues, Thread-safe Value Types, and Avoiding Race Conditions

--- NetworkingModule/Feed Cache/FeedStore.swift
+++ NetworkingModule/Feed Cache/FeedStore.swift
@@ -17,7 +17,16 @@ protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrieveCompletion = (RetrieveCachedFeedResult) -> Void
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func retrieve(completion: @escaping RetrieveCompletion)
 }
