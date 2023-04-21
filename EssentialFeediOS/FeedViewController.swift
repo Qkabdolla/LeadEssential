706243179a827f647d71aa7060202cf64eb3e36b
Kabdolla <Madiyar.Kabdolla@kaspi.kz>
2023-04-21 16:20:13 +0600

    Apple MVC, Test-driving UIViewControllers, Dealing with UIKit’s Inversion of Control & Temporal Coupling, and Decoupling Tests from UI Implementation Details

---
+++ EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeedViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Каспи on 21.04.2023.
+//
+
+import UIKit
+import EssentialFeed
+
+final class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    
+    convenience init(loader: FeedLoader?) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
