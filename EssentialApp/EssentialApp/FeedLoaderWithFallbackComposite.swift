fde5514f4bc553d4e7ecf489130560de35a400e9
Kabdolla <Madiyar.Kabdolla@kaspi.kz>
2023-06-10 18:40:15 +0600

    Composite Pattern: Implementing a Flexible & Composable Strategy for Loading Data with Fallback Logic

---
+++ EssentialApp/EssentialApp/FeedLoaderWithFallbackComposite.swift
@@ -0,0 +1,26 @@
+//
+//  FeedLoaderWithFallbackComposite.swift
+//  EssentialApp
+//
+//  Created by Каспи on 10.06.2023.
+//
+
+import EssentialFeed
+
+class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+    private let fallback: FeedLoader
+    init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        primary.load { [weak self] result in
+            switch result {
+            case .success: completion(result)
+            case .failure: self?.fallback.load(completion: completion )
+            }
+        }
+    }
+}
